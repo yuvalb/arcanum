92f8179035a431ca0f10eb9def30149e8284919b
yuvalb <drakemare@gmail.com>
2021-01-30 15:21:27 +0200

    node-ts-cli-yargs and semantic fixes (#3)

---
+++ skeletons/node-ts-cli-yargs/README.md
@@ -0,0 +1,15 @@
+# Node Typescript CLI skeleton
+A minimal typescript node CLI application leveraging `ts-node`, with `yargs` argument parser.
+
+## Usage
+### Local
+```
+npm i
+npm start -- -b "hello world!"
+```
+
+### CLI
+```
+npm i - g
+node-ts-cli -b "hello world!"
+```
---
+++ skeletons/node-ts-cli-yargs/lib/cli.ts
@@ -0,0 +1,24 @@
+import yargs from 'yargs/yargs';
+
+interface Arguments {
+  [x: string]: unknown;
+  a: boolean;
+  b: string;
+  c: number | undefined;
+  d: (string | number)[] | undefined;
+  e: number;
+  f: string | undefined;
+}
+
+export default async function () {
+    const argv: Arguments = yargs(process.argv.slice(2)).options({
+      a: { type: 'boolean', default: false },
+      b: { type: 'string', demandOption: true },
+      c: { type: 'number', alias: 'chill' },
+      d: { type: 'array' },
+      e: { type: 'count' },
+      f: { choices: ['1', '2', '3'] }
+    }).argv;
+
+    console.log("Received arguments: ", argv);
+}
